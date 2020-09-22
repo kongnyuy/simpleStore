edf45f17e1989c66c730ac3f8b69ffe27e246db5
Tata BDK <briankong29@gmail.com>
2020-09-22 03:47:45 +0100

    include cart items structure

--- server/store/static/inventory/scripts/js/cart.js
+++ server/store/static/inventory/scripts/js/cart.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', ev => {
             console.log('close cart dialog')
             cartModal.classList.remove(isActive)
             cartModal.classList.remove(isClipped)
-            cart_modal_open = false                        
+            cart_modal_open = false
         }
     })
 
@@ -46,6 +46,15 @@ document.addEventListener('DOMContentLoaded', ev => {
         }
     })
 
+    let hideBtn = document.querySelector('.hide-btn')
+    hideBtn.addEventListener('click', ev => {
+        console.log('hide btn')
+        let parent = ev.target.parentNode
+        if(parent){
+            parent.style.display = 'none'
+        }
+    })
+
     //prefill cart view elments
     function updateCartCountView() {
         if (window.cart) {
@@ -55,41 +64,129 @@ document.addEventListener('DOMContentLoaded', ev => {
         }
     }
 
+
+    let citInc = document.querySelector('#cit-inc'),
+        citDec = document.querySelector('#cit-dec');
+
+    citDec.addEventListener('click', ev => {
+        console.log('deduct')
+        //this.decrement()
+    })
+    citInc.addEventListener('click', ev => {
+        console.log('Increment')
+        //this.increment()
+    })
+
+
+
 })
 
 var cartItemTpl = `
 `
 
+function CartItem(item = { quantity: 0 }) {
+    this.item = item
+    this.quantity = parseInt(this.item.quantity)
+
+    this.totalPrice = function () {
+        return this.quantity * (parseFloat(this.item.cost) || 0)
+    }
+
+   
+    this.tpl = `<tr>
+                            <td>${item.name}</td>
+                            <td>${item.cost}</td>
+                            <td>${item.quantity}</td>
+                            <td>${this.totalPrice()}</td>
+                            <td>${this.isAvail ? 'yes' : 'no'}</td>
+                            <td>
+                                <div class="field has-addons">
+                                    <p class="control">
+                                        <button class="button is-primary  is-small">                                            
+                                            <span>Add</span>
+                                        </button>
+                                    </p>
+                                    <p class="control">
+                                        <button id="cit-inc" class="button  is-small">                                            
+                                            <span>Deduct</span>
+                                        </button>
+                                    </p>
+                                    <p class="control">
+                                        <button id="cit-dec" class="button is-danger is-small">                                            
+                                            <span>Delete</span>
+                                        </button>
+                                    </p>
+                                </div>
+                            </td>
+
+                        </tr>`        
+
+
+    this.getNode = function () {
+        return this.tpl
+    }
+    
+
+
+    this.emitCustomClickEvent = function(evName = '', data = {}){
+        const ev = new CustomEvent(evName, {
+            bubbles: true,
+            detail: { ...data }
+        });
+    }
+
+    this.increment = function () {
+        console.info('>>> increment')
+        if (this.quantity < item.quantity) {
+            this.quantity++            
+        }
+    }
+
+    this.decrement = function () {
+        console.info('>>> decrement')
+        if (this.quantity > 0) {
+            this.quantity--
+        } else {
+            this.quantity = 0
+        }
+    }
+
+
+    this.initHandlers = function () {
+/*         citDec.addEventListener('click', ev => {
+            console.log('deduct')
+            this.decrement()
+        })
+        citInc.addEventListener('click', ev => {
+            console.log('Increment')
+            this.increment()
+        }) */
+    }
+
+    this.initHandlers()
+
+    return this
+}
+
 function fillCartItemsView() {
     console.info('fill cart items')
-    let area = document.querySelector('.mcb-articles')
+    //let area = document.querySelector('.mcb-articles')
+    let area = document.querySelector('tbody#items-table-body')
+    console.log(area)
     if (area) {
-        area.innerHTML = ''
+        //area.innerHTML = ''
         if (window.cart) {
-            let out = '', i = 0        
+            let out = '', i = 0
             window.cart.getItems().forEach((entry) => {
                 i++;
-                out += `<div key=${i} class="cit">
-    <div class="cit-val">
-        <b class="cit-val-name">${entry.name}</b>
-    </div>
-    <div class="cit-controls">        
-        <button class="button is-danger ">
-            <span class="icon is-small">
-                <i class=" gg-add"></i>
-            </span>
-        </button>
-        <b id="cit-count">100</b>
-        <button class="button is-info ">
-            <span class="icon is-small">
-                <i class=" gg-remove"></i>                
-            </span>
-        </button>
-    </div>
-</div>`
+                ct = CartItem(entry)
+                console.log(ct)
+                out += ct.getNode()
             })
             console.log(out)
             area.innerHTML = out
+        }else{
+            console.error('not there')
         }
     }
 }
